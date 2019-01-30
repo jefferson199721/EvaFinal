733bd4201a23067f88714a05d5698c82b6c4e7b2
juanitoj <juanitojfm56@gmail.com>
2019-01-30 18:29:48 -0500

    Modelo examen

---
+++ EvaFinal/EvaFinal/Models/Examen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaFinal.Models
+{
+    public class Examen
+    {
+        public int ExamenId { get; set; }
+        public string Preguntas { get; set; }
+        public string Fecha { get; set; }
+
+
+
+    }
+}
