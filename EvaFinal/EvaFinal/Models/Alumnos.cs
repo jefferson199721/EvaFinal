08e20d349f1b97ac401c4df09403753d9d0a4cd8
jefferson199721 <jeffersonmvinueza@gmail.com>
2019-01-30 18:06:56 -0500

    alum

--- EvaFinal/EvaFinal/Models/Alumnos.cs
+++ EvaFinal/EvaFinal/Models/Alumnos.cs
@@ -11,5 +11,8 @@ namespace EvaFinal.Models
         public string Nombres { get; set; }
         public string Apellidos { get; set; }
         public string Telefono { get; set; }
+        public string Correo { get; set; }
+      
+
     }
 }
