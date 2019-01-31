701186d8e3c35d3d32a5925f51d1ac77e52c7c91
xavier <xavicoeduar.p@hotmail.com>
2019-01-31 14:10:18 -0500

    Realiza

---
+++ EvaFinal/EvaFinal/Models/realiza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaFinal.Models
+{
+    public class realiza
+    {
+        public int realizaId { get; set; }
+        public int AlumnosId { get; set; }
+        public int PracticaId { get; set; }
+        public string fecha { get; set; }
+        public string nota { get; set; }
+    }
+}
