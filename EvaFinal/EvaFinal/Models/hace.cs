25bb5dc6a8ff280e9d3084aafd773619198f2983
ericko_1709 <macara.123@hotmail.com>
2019-01-31 12:10:23 -0500

    Este

---
+++ EvaFinal/EvaFinal/Models/hace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaFinal.Models
+{
+    public class hace
+    {
+        [Key]
+        public int hacerid { get; set; }
+        public int NumMatricula  { get; set; }
+        public Alumnos alumnos { get; set; }
+        public int ExamenId { get; set; }
+        public Examen examen { get; set; }
+        public string Nota { get; set; }
+        
+    }
+}
