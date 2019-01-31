274499bea07cc891bf52b6fa5878478363ddc7b4
juanitoj <juanitojfm56@gmail.com>
2019-01-31 15:20:00 -0500

    Modificar

--- EvaFinal/EvaFinal/Data/Migrations/20190131201401_hhhcorregido.Designer.cs
+++ EvaFinal/EvaFinal/Data/Migrations/20190131201401_hhhcorregido.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EvaFinal.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190131201401_hhhcorregido")]
+    partial class hhhcorregido
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
