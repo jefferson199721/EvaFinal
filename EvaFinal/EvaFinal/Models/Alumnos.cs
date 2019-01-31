aaa74fca93788e9d3f55e4c1273fd36c2c7035e5
jefferson199721 <jeffersonmvinueza@gmail.com>
2019-01-30 20:27:25 -0500

    alumnos

--- EvaFinal/EvaFinal/Models/Alumnos.cs
+++ EvaFinal/EvaFinal/Models/Alumnos.cs
@@ -10,10 +10,8 @@ namespace EvaFinal.Models
         public int NumMatricula { get; set; }
         public string Nombres { get; set; }
         public string Apellidos { get; set; }
-        public string Telefono { get; set; }
-        public string Correo { get; set; }
-        public string Direccion { get; set; }
-        public string Sexo { get; set; }
+        public string CursoParalelo { get; set; }
+      
       
 
     }
