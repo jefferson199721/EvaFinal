6ad90f4cf8e5e1ebcd160c373d131a1341945233
heisemverg <mshermesjr@gmail.com>
2019-01-30 23:23:28 -0500

    Profesor Modelo

---
+++ EvaFinal/EvaFinal/Models/Profesor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvaFinal.Models
+{
+    public class Profesor
+    {
+        public int ProfesorId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
