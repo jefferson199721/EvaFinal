df94569fce73978363933d47882535b9531dfa5b
ericko_1709 <macara.123@hotmail.com>
2019-01-31 14:08:46 -0500

    Update haceModel.cs
    return

--- EvaFinal/EvaFinal/Models/haceModel.cs
+++ EvaFinal/EvaFinal/Models/haceModel.cs
@@ -57,7 +57,7 @@ namespace EvaFinal.Models
 
         public hace Un_Hace_Model(int hacerid)
         {
-//asssaa
+//return 
             hace hace = (from hc in _contexto.Hacer
                                where hc.hacerid == hacerid
                             select hc).FirstOrDefault();
